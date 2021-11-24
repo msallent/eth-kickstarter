1145d51d39fd1a5d7206a6c3fcf5cee730c50d39
Matias Sallent <mqsallent@gmail.com>
2021-11-24 14:05:40 -0300

    Show cards for each campaign

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import type { NextPage } from 'next';
+import { useMemo } from 'react';
+import { Card, CardProps, Header } from 'semantic-ui-react';
 import { campaignFactory } from '../utils/campaignFactory';
 
 interface HomeProps {
@@ -6,7 +8,26 @@ interface HomeProps {
 }
 
 const Home: NextPage<HomeProps> = ({ campaigns }) => {
-  return <div>Campaigns: {campaigns.length}</div>;
+  const cardItems = useMemo<Array<CardProps>>(
+    () =>
+      campaigns.map((campaign, index) => ({
+        header: campaign,
+        meta: `Campaign #${index + 1}`,
+        description: <a>View Campaign</a>,
+        image: '/eth-landscape.png',
+        fluid: true,
+      })),
+    [campaigns]
+  );
+
+  return (
+    <>
+      <Header size="large" inverted>
+        Active Campaigns:
+      </Header>
+      <Card.Group items={cardItems} itemsPerRow="2" />
+    </>
+  );
 };
 
 export const getServerSideProps = async () => {
