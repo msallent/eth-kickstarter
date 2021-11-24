00a282e129dfdb2a4c2ac7585d296fb5fb55cf7f
Matias Sallent <mqsallent@gmail.com>
2021-11-24 16:46:24 -0300

    Add page to create new campaigns

---
+++ src/pages/campaigns/new.tsx
@@ -0,0 +1,57 @@
+import type { NextPage } from 'next';
+import { ChangeEvent, useState } from 'react';
+import { Button, Form, Icon, Input, Message } from 'semantic-ui-react';
+import { campaignFactory } from '../../utils/campaignFactory';
+import { web3 } from '../../utils/web3';
+
+const NewCampaign: NextPage = () => {
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [minimumContribution, setMinimumContribution] = useState('');
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setMinimumContribution(event.target.value);
+    setError('');
+  };
+
+  const onSubmit = async () => {
+    setError('');
+    setIsLoading(true);
+
+    try {
+      const [account] = await web3.eth.getAccounts();
+      await campaignFactory.methods.createCampaign(minimumContribution).send({ from: account });
+    } catch (error: any) {
+      setError(error.message);
+    }
+
+    setIsLoading(false);
+  };
+
+  return (
+    <>
+      <Message
+        attached
+        header="Create a new campaign"
+        content="Fill out the form below to create a new campaign"
+      />
+      <Form onSubmit={onSubmit} className="attached fluid segment">
+        <Form.Field>
+          <label>Minimum contribution</label>
+          <Input type="number" label="wei" labelPosition="right" onChange={onChange} />
+        </Form.Field>
+        <Button primary disabled={!minimumContribution || isLoading} loading={isLoading}>
+          Create
+        </Button>
+      </Form>
+      {(!minimumContribution || error) && (
+        <Message error={!!error} warning={!minimumContribution} attached="bottom">
+          {error ? <Icon name="delete" /> : <Icon name="warning" />}
+          {error || 'Please fill out the form'}
+        </Message>
+      )}
+    </>
+  );
+};
+
+export default NewCampaign;
