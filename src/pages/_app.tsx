371b59adda8752620640ba214da1528f5f9c9b80
Matias Sallent <mqsallent@gmail.com>
2021-11-24 22:09:01 -0300

    Add title to pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { Layout } from '../components/Layout/Layout';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <title>ETH Kickstarter</title>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
